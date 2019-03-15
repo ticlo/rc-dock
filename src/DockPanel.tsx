d1d94b63106537776ab24160b2ff5427a6732ad9
Rick Zhou <rinick@gmail.com>
2019-03-15 00:59:25 -0700

    work on Panel drag drop icons

--- src/DockPanel.tsx
+++ src/DockPanel.tsx
@@ -14,16 +14,40 @@ interface State {
 
 export class DockPanel extends React.PureComponent<Props, State> {
 
+  static _droppingPanel: DockPanel;
+  static set droppingPanel(panel: DockPanel) {
+    if (DockPanel._droppingPanel === panel) {
+      return;
+    }
+    if (DockPanel._droppingPanel) {
+      DockPanel._droppingPanel.onDragLeave();
+    }
+  }
+
+  onDragOver = () => {
+    DockPanel.droppingPanel = this;
+    this.setState({dropping: null});
+  };
+
+  onDragLeave() {
+    this.setState({dropping: null});
+  }
+
   render(): React.ReactNode {
+    let {dropping} = this.state;
     let {panelData, size} = this.props;
     let {minWidth, minHeight, group} = panelData;
     let {panelClass} = group;
-    let cls = 'dock-panel';
-    if (panelClass) {
-      cls = `dock-panel ${panelClass}`;
+
+    let cls = `dock-panel ${panelClass ? panelClass : ''} ${dropping ? 'dock-panel-dropping' : ''}`;
+
+    let droppingLayer: React.ReactNode;
+    if (dropping) {
+
     }
+
     return (
-      <div className={cls} style={{minWidth, minHeight, flex: `1 1 ${size}px`}}>
+      <div className={cls} style={{minWidth, minHeight, flex: `1 1 ${size}px`}} onDragOver={this.onDragOver}>
         <DockTabs panelData={panelData}/>
       </div>
     );

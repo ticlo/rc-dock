4eb3091429041f8ee775c7da9133a77ac3ea9482
Rick Zhou <rinick@gmail.com>
2019-03-18 00:25:47 -0700

    remove lib sub folders

--- lib/src/DragStore.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class DragStore {
-    static dragStart(scope: any, data: {
-        [key: string]: any;
-    }, element?: any): void;
-    static getData(scope: any, field: string): any;
-    static dragEnd(): void;
-}

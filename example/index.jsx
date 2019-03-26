8c12c8ccc79eee7d28ea2c82a1ab0ddcffcd0638
Rick Zhou <rinick@gmail.com>
2019-03-25 21:48:22 -0700

    add tab-update example

--- example/index.jsx
+++ example/index.jsx
@@ -2,20 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Divider} from '../lib';
 
-let demos = ['basic', 'panel-style', 'tab-cache', 'tab-min-size', 'divider'];
+let demos = ['basic', 'panel-style', 'tab-cache', 'tab-min-size', 'tab-update', 'divider'];
+let advance = ['standalone-divider'];
 
 class App extends React.Component {
   state = {current: 'basic'};
 
   render() {
     let {current} = this.state;
-    let children = [];
+    let demoPages = [];
     for (let page of demos) {
       let cls = '';
       if (page === current) {
         cls = 'current';
       }
-      children.push(
+      demoPages.push(
+        <a href={`#${page}`} className={cls} onClick={(e) => this.setState({current: page})}>
+          {page}
+        </a>
+      )
+    }
+    let advancePages = [];
+    for (let page of advance) {
+      let cls = '';
+      if (page === current) {
+        cls = 'current';
+      }
+      advancePages.push(
         <a href={`#${page}`} className={cls} onClick={(e) => this.setState({current: page})}>
           {page}
         </a>
@@ -25,7 +38,11 @@ class App extends React.Component {
       <div>
         <div className='link-bar'>
           Examples:
-          {children}
+          {demoPages}
+        </div>
+        <div className='link-bar'>
+          Advanced:
+          {advancePages}
         </div>
         <iframe src={`./${current}.html`}/>
       </div>

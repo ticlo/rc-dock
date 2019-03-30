f5dabb540cc886fd680dc314c25f377542eed4d4
Rick Zhou <rinick@gmail.com>
2019-03-29 23:44:08 -0700

    add mobile-drag-drop in example

--- example/shared-import.js
+++ example/shared-import.js
@@ -2,6 +2,9 @@ export * from '../lib';
 
 import React1 from 'react';
 import ReactDom1 from 'react-dom';
+import {polyfill} from "mobile-drag-drop";
 
 export const React = React1;
 export const ReactDOM = ReactDom1;
+
+polyfill({});

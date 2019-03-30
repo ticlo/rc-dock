ae1b8e42938abe1e336c37a72e155485b6c39d73
Rick Zhou <rinick@gmail.com>
2019-03-29 23:57:37 -0700

    Revert "add mobile-drag-drop in example"
    This reverts commit f5dabb540cc886fd680dc314c25f377542eed4d4.

--- example/shared-import.js
+++ example/shared-import.js
@@ -2,9 +2,6 @@ export * from '../lib';
 
 import React1 from 'react';
 import ReactDom1 from 'react-dom';
-import {polyfill} from "mobile-drag-drop";
 
 export const React = React1;
 export const ReactDOM = ReactDom1;
-
-polyfill({});

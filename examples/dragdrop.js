16a636790fdf196bdbaec4742c094dd9dab6d2ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-08 04:01:29 +0000

    Deploy ticlo/rc-dock to github.com/ticlo/rc-dock.git:gh-pages

--- examples/dragdrop.js
+++ examples/dragdrop.js
@@ -9758,7 +9758,7 @@ class DockPanel extends react_1.default.PureComponent {
       this.onPanelCornerDrag(e, 'br');
     };
 
-    this.onPanelCornerDragMove = (e, dx, dy) => {
+    this.onPanelCornerDragMove = e => {
       let {
         panelData
       } = this.props;
@@ -9766,33 +9766,33 @@ class DockPanel extends react_1.default.PureComponent {
       switch (this._movingCorner) {
         case 'tl':
           {
-            panelData.x = this._movingX + dx;
-            panelData.w = this._movingW - dx;
-            panelData.y = this._movingY + dy;
-            panelData.h = this._movingH - dy;
+            panelData.x = this._movingX + e.dx;
+            panelData.w = this._movingW - e.dx;
+            panelData.y = this._movingY + e.dy;
+            panelData.h = this._movingH - e.dy;
             break;
           }
 
         case 'tr':
           {
-            panelData.w = this._movingW + dx;
-            panelData.y = this._movingY + dy;
-            panelData.h = this._movingH - dy;
+            panelData.w = this._movingW + e.dx;
+            panelData.y = this._movingY + e.dy;
+            panelData.h = this._movingH - e.dy;
             break;
           }
 
         case 'bl':
           {
-            panelData.x = this._movingX + dx;
-            panelData.w = this._movingW - dx;
-            panelData.h = this._movingH + dy;
+            panelData.x = this._movingX + e.dx;
+            panelData.w = this._movingW - e.dx;
+            panelData.h = this._movingH + e.dy;
             break;
           }
 
         case 'br':
           {
-            panelData.w = this._movingW + dx;
-            panelData.h = this._movingH + dy;
+            panelData.w = this._movingW + e.dx;
+            panelData.h = this._movingH + e.dy;
             break;
           }
       }
@@ -9932,19 +9932,23 @@ class DockPanel extends react_1.default.PureComponent {
     }), isFloat ? [react_1.default.createElement(DragDropDiv_1.DragDropDiv, {
       key: 'drag-size-t-l',
       className: 'dock-panel-drag-size dock-panel-drag-size-t-l',
-      onDragStartT: this.onPanelCornerDragTL
+      onDragStartT: this.onPanelCornerDragTL,
+      onDragMoveT: this.onPanelCornerDragMove
     }), react_1.default.createElement(DragDropDiv_1.DragDropDiv, {
       key: 'drag-size-t-r',
       className: 'dock-panel-drag-size dock-panel-drag-size-t-r',
-      onDragStartT: this.onPanelCornerDragTR
+      onDragStartT: this.onPanelCornerDragTR,
+      onDragMoveT: this.onPanelCornerDragMove
     }), react_1.default.createElement(DragDropDiv_1.DragDropDiv, {
       key: 'drag-size-b-l',
       className: 'dock-panel-drag-size dock-panel-drag-size-b-l',
-      onDragStartT: this.onPanelCornerDragBL
+      onDragStartT: this.onPanelCornerDragBL,
+      onDragMoveT: this.onPanelCornerDragMove
     }), react_1.default.createElement(DragDropDiv_1.DragDropDiv, {
       key: 'drag-size-b-r',
       className: 'dock-panel-drag-size dock-panel-drag-size-b-r',
-      onDragStartT: this.onPanelCornerDragBR
+      onDragStartT: this.onPanelCornerDragBR,
+      onDragMoveT: this.onPanelCornerDragMove
     })] : null, droppingLayer);
   }
 

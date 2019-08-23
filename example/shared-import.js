6d2915495be85bf718af46932300cff542381de9
Rick Zhou <rinick@gmail.com>
2019-08-23 00:18:17 -0700

    fix new-window example

--- example/shared-import.js
+++ example/shared-import.js
@@ -4,8 +4,7 @@ export * from './prism-tabs';
 import React1 from 'react';
 import ReactDom1 from 'react-dom';
 
-import NewWindow1 from 'react-new-window';
+export NewWindow from 'react-new-window';
 
 export const React = React1;
 export const ReactDOM = ReactDom1;
-export const NewWindow = NewWindow1;

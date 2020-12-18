c9328b73cb69f59a9e61967369a076938e69bf9f
rinick <rinick@gmail.com>
2020-12-18 01:23:22 -0800

    fix example compilation

--- example/shared-import.js
+++ example/shared-import.js
@@ -4,7 +4,5 @@ export * from './prism-tabs';
 import React1 from 'react';
 import ReactDom1 from 'react-dom';
 
-export NewWindow from 'react-new-window';
-
 export const React = React1;
 export const ReactDOM = ReactDom1;

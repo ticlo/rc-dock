12ae7c83858eb1a61788a533f089254aadc3e4c7
rinick <rinick@gmail.com>
2021-08-09 02:04:27 -0700

    example for dragging new tab

---
+++ example/drag-new-tab.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {htmlTab, jsxTab} from "./prism-tabs";
+import {DockLayout, DragDropDiv} from '../lib';
+import {getFloatPanelSize} from "../src/Algorithm";
+
+let tab = {
+  title: 'Tab',
+  content: <div>Tab Content</div>
+};
+let box = {
+  dockbox: {
+    mode: 'horizontal',
+    children: [
+      {
+        mode: 'vertical',
+        children: [
+          {
+            tabs: [jsxTab, htmlTab],
+          },
+          {
+            tabs: [{...tab, id: 't4'}, {...tab, id: 't5'}, {...tab, id: 't6'}],
+          }
+        ]
+      },
+      {
+        tabs: [{...tab, id: 't7'}, {...tab, id: 't8'}, {...tab, id: 't9'}]
+      },
+    ]
+  }
+};
+
+class Demo extends React.Component {
+  newTabId = 0;
+  getRef = (r) => {
+    this.dockLayout = r;
+  };
+  getButtonRef = (r) => {
+    this.buttonRef = r;
+  }
+
+  onDragStart = (e) => {
+    console.log(this.dockLayout, this.buttonRef)
+    e.setData({
+      tab: {...tab, id: `newTab-${++this.newTabId}`},
+      panel: { // panel object
+        parent: {},
+        // group: '', // define your tab group here
+      },
+      panelSize: [400, 300]
+    }, this.dockLayout.getDockId());
+    e.startDrag(this.buttonRef.element, this.buttonRef.element);
+  };
+
+  render() {
+    return (
+      <div>
+        <DockLayout ref={this.getRef} defaultLayout={box}
+                    style={{position: 'absolute', left: 10, top: 60, right: 10, bottom: 10}}/>
+        <div className='top-panel'>
+          Save Layout:
+          <DragDropDiv ref={this.getButtonRef} onDragStartT={this.onDragStart}>
+            <button className='btn'>
+              Drag Me to Create New Tab
+            </button>
+          </DragDropDiv>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Demo/>, document.getElementById('app'));
--- example/index.jsx
+++ example/index.jsx
@@ -4,7 +4,7 @@ import {Divider} from '../lib';
 // keep the above unused import so tools script can understand this jsx
 
 let demos = ['basic', 'panel-style', 'drop-mode', 'tab-update', 'save-layout', 'panel-extra'];
-let advance = ['new-window', 'adv-tab-update', 'adv-save-layout', 'controlled-layout', 'tab-cache', 'divider-box'];
+let advance = ['new-window', 'adv-tab-update', 'adv-save-layout', 'controlled-layout', 'tab-cache', 'divider-box', 'drag-new-tab'];
 
 let defaultPage = window.location.hash.substr(1);
 if (!(demos.includes(defaultPage) || advance.includes(defaultPage))) {
